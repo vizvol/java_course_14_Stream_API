dec670f3e86a915a64905ae0143488c6dbd9793c
izvol <v.izvol@iteco-inno.ru>
2022-05-10 13:07:36 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,61 @@
 import javax.sound.midi.Soundbank;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<User> personList = new ArrayList<>(Arrays.asList(
+        List<User> userList = new ArrayList<>(Arrays.asList(
                 new User("Joe Louis", 15, 2008),
                 new User("Evander Holyfield", 12, 2010),
                 new User("Vladimir Klichko", 32, 1990),
                 new User("Tyson Fury", 11, 2011),
                 new User("Oleksandr Usyk", 30, 1992),
                 new User("Mike Tyson", 27, 1995),
-                new User(" Muhammad Ali", 29, 1993)
+                new User("Muhammad Ali", 27, 1995)
         ) );
-
-        for (User user : personList) {
+        /*System.out.println("\nКоллекция:");
+        for (User user : userList) {
             System.out.println(user);
-        }
-        System.out.println("Вывод отсортированной коллекции по возрастанию: ");
+        }*/
+        //Stream<User> stream = personList.stream();
+        System.out.println("Коллекция:");
+        userList.stream().forEach(System.out::println );
 
-        System.out.println("Вывод отсортированной коллекции по убыванию: ");
+        System.out.println("\nВывод отсортированной коллекции по возрастанию (по полю YEAR и NAME ): ");
+        userList.stream().sorted(Comparator.comparing(User::getYear).thenComparing(User::getName)).forEach(System.out::println);
 
-        System.out.println("Фильтрация коллекции по полю AGE: ");
+        System.out.println("\nВывод отсортированной коллекции по убыванию: (по полю YEAR) ");
+        userList.stream().sorted(Comparator.comparing(User::getYear).reversed() ).forEach(System.out::println);
 
-        System.out.println("Сортировка коллекции по полю YEAR и группировка по этому полю . ");
+        System.out.println("\nВывод всех несовершеннолетних пользователей, рожденных с 2010г. (фильтрация коллекции) : ");
+        userList.stream().filter(x -> x.getAge() < 18).filter(x -> x.getYear() >= 2010).forEach(System.out::println);
 
-        System.out.println("Минимальный объект коллекции: ");
+        System.out.println("\nСортировка коллекции по полю YEAR и группировка по этому полю . ");
+        Map<String, List<User>> hashMap =
+                userList.stream().sorted(Comparator.comparing(User::getYear)).collect(Collectors.groupingBy(x -> x.getYear() < 2000 ? "до 2000" : "c 2000") ) ;
+        hashMap.forEach((k, v) -> {
+                    System.out.println("год рождения: " + k);
+                    for (User user : v) {
+                        System.out.println(user);
+                    }
+                } );
 
-        System.out.println("Максимальный объект коллекции: ");
+        System.out.println("\nМинимальный объект коллекции по AGE: ");
+        userList.stream().min(Comparator.comparing(User::getAge)).ifPresent(System.out::println);
 
-        System.out.println("Использование AllMath :");
+        System.out.println("\nМаксимальный объект коллекции по AGE:  ");
+        userList.stream().max(Comparator.comparing(User::getAge)).ifPresent(System.out::println);
 
-        System.out.println("Использование AnyMath :");
+        System.out.println("\nИспользование AllMath : ");
+        System.out.println("Все пользователи старше 10 лет : " + userList.stream().allMatch(x -> x.getAge() > 10));
 
-        System.out.println("Использование NoneMath :");
+        System.out.println("\nИспользование AnyMath :");
+        System.out.println("Есть ли кто-то старше 30 лет : " + userList.stream().anyMatch(x -> x.getAge() > 30));
+
+        System.out.println("\nИспользование NoneMath : ");
+        System.out.println("Никто не родился раньше 1990 года: " + userList.stream().noneMatch(x -> x.getYear () < 1990));
 
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,12 +1,51 @@
-public class User {
-    String name;
-    int age;
-    int year;
+import java.util.Objects;
 
-    public User(String name, int age, int year) {
+public class User {
+    private String name;
+    private Integer age;
+    private Integer year;
+
+    public User(String name, Integer age, Integer year) {
         this.name = name;
         this.age = age;
         this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(age, user.age) && Objects.equals(year, user.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, year);
     }
 
     @Override
